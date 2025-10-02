a94ef7ea012d3580e140eb8fcfbfe2186b8fefa7
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-10-02 10:49:29 -0400

    Initial Build
    This should be a default build

---
+++ addons/main/config.cpp
@@ -0,0 +1,23 @@
+#include "script_version.hpp"
+
+class CfgPatches
+{
+	class bzn_main
+	{
+		name="main";
+		author="Buzan Consortium Mod Team";
+		requiredAddons[]={};
+		VERSION_CONFIG;
+	};
+};
+
+class CfgSettings
+{
+	class CBA{
+		class versioning {// https://github.com/CBATeam/CBA_A3/wiki/Versioning-System
+			class bzn_main {
+				main_addon = "bzn_main";
+			};
+		};
+	};
+};
---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "Buzan Consortium Aux Mod";
+picture = "Buzan.paa";
+overview = "The aux mod for Buzan Consortium.<br/>You probably only need this if you are in our unit.";
+logo = "Buzan.paa";
+logoOver = "Buzan.paa";
+actionName = "GitHub";
+action = "https://github.com/12TrashPandas/BuzanConsortium";
