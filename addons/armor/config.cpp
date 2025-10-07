98cbdbe28539c2aa8b762064d305ce3ddbc103c5
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-10-06 20:06:58 -0400

    uniform update
    need to do changelog and maritime uniforms, adds 2 new uniform variants in 5 camos for men, and 1 new uniform in 5 camos for women.

---
+++ addons/armor/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches
+{
+	class bzn_armor
+	{
+		author="Bzn Mod Dev Team, Yandere";
+		requiredAddons[]=
+		{
+			"PUP_CA_U", //CAF 2035
+		};
+	};
+};
+
+class CfgUnitInsignia
+{
+	class Bzn_Consortium
+	{
+		displayName="Buzan Consortium";
+		author="Yandere";
+		texture="\z\bzn\addons\armor\data\BuzanPatch.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+	};
+};
